d9715ba9cb41d0c9789c026f0174d2218d62edb4
Mohd Yasir Khan <khanmohdyasir07@gmail.com>
2025-11-23 15:24:20 +0530

    Update and rename Q05.c to Q005.c

---
+++ Q005.c
@@ -0,0 +1,31 @@
+<<<<<<< HEAD
+#include <stdio.h>
+
+int main() {
+    float c, f;
+
+    printf("Enter temperature in Celsius: ");
+    scanf("%f", &c);
+
+    f = (c * 9/5) + 32;
+
+    printf("Temperature in Fahrenheit: %.2f\n", f);
+
+    return 0;
+}
+=======
+#include <stdio.h>
+
+int main() {
+    float c, f;
+
+    printf("Enter temperature in Celsius: ");
+    scanf("%f", &c);
+
+    f = (c * 9/5) + 32;
+
+    printf("Temperature in Fahrenheit: %.2f\n", f);
+
+    return 0;
+}
+>>>>>>> 3984c52b78e980578c5bf104cd4754c88d3c7e43
