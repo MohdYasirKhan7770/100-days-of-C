f931efd61535b3826efef802321390fda94ee332
Mohd Yasir Khan <khanmohdyasir07@gmail.com>
2025-11-23 15:23:19 +0530

    Update and rename Q04.c to Q004.c

---
+++ Q004.c
@@ -0,0 +1,35 @@
+<<<<<<< HEAD
+#include <stdio.h>
+
+int main() {
+    float radius, area, circumference;
+
+    printf("Enter radius: ");
+    scanf("%f", &radius);
+
+    area = 3.14 * radius * radius;
+    circumference = 2 * 3.14 * radius;
+
+    printf("Area: %.2f\n", area);
+    printf("Circumference: %.2f\n", circumference);
+
+    return 0;
+}
+=======
+#include <stdio.h>
+
+int main() {
+    float radius, area, circumference;
+
+    printf("Enter radius: ");
+    scanf("%f", &radius);
+
+    area = 3.14 * radius * radius;
+    circumference = 2 * 3.14 * radius;
+
+    printf("Area: %.2f\n", area);
+    printf("Circumference: %.2f\n", circumference);
+
+    return 0;
+}
+>>>>>>> 3984c52b78e980578c5bf104cd4754c88d3c7e43
